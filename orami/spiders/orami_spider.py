f4234c87d47624cbe28d037fa7dbfac8c14cd6ba
ajikusbandonoext <ajikusbandonoext@gmail.com>
2020-08-09 10:26:40 +0700

    update

--- orami/spiders/orami_spider.py
+++ orami/spiders/orami_spider.py
@@ -16,32 +16,32 @@ class OramiSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://www.orami.co.id/ap/takoyakids',
-            'https://www.orami.co.id/c/peralatan-bayi',
-            'https://www.orami.co.id/c/fashion-dan-aksesoris'
-        ]
+            'https://www.orami.co.id/',
+            ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.getlink)
 
+    def getlink(self, response):
+        links = response.xpath("//div[contains(@class, 'oss-u-1-8 pl-8 pr-8')]/div/a/@href").extract()
+        
+        for link in links:
+            try:
+                yield scrapy.Request(url=link, callback=self.parse)
+            except:
+                _link =  'https://www.orami.co.id' + link
+                yield scrapy.Request(url=_link, callback=self.parse)
+     
     def parse(self, response):
-        # results = response.xpath("//button[contains(@onclick, 'pushOnclick')]/@onclick").extract()
-        # words = [w.replace('pushOnclick(', '').replace(');', '') for w in results]
-        # for w in results:
-        #     item = w.replace('pushOnclick(', '').replace(');', '').replace("'", '').split(',')
-        #     scraped_info = {
-        #             'ID' : item[0],
-        #             'NAMA PRODUK' : item[1],
-        #             'URL' : item[4],
-        #             'HARGA' : item[2],
-        #             }
-        #     yield scraped_info
+        
+        _res = response.url
+        _price = response.xpath("//div[contains(@class, 'widget-price mb-8')]//p/text()").extract()
+        _name_product = response.xpath("//div[contains(@class, 'prod-name mb-8')]//a/text()").extract()
 
-        price = response.xpath("//div[contains(@class, 'widget-price mb-8')]//p/text()").extract()
-        name_product = response.xpath("//div[contains(@class, 'prod-name mb-8')]//a/text()").extract()
-
-        for item in zip(price,name_product):
+        for _item in zip(_price,_name_product):
             scraped_info = {
-                'price' : item[0],
-                'name_product' : item[1].lstrip().rstrip(),
+                'price' : _item[0],
+                'name_product' : _item[1].lstrip().rstrip(),
+                'link' : _res
                 }  
-            yield scraped_info          
+            yield scraped_info
+             
